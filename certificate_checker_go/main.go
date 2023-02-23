d21eacde1aaaad7fb9d4dd53076b184e81a2a7e3
James Chung <james@chung.contact>
2023-02-22 19:45:45 -0600

    feat: add go version (#1)

---
+++ certificate_checker_go/main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+)
+
+var (
+	lambdaStart     = lambda.Start
+	ErrDomainName   = errors.New("DOMAIN_NAME is not defined")
+	ErrSNSTopicARN  = errors.New("SNS_TOPIC_ARN is not defined")
+	ErrBufferInDays = errors.New("BUFFER_IN_DAYS is not defined")
+)
+
+type SNSPublish interface {
+	Publish(context.Context, *sns.PublishInput, ...func(*sns.Options)) (*sns.PublishOutput, error)
+}
+
+type Env struct {
+	DomainName   string
+	SNSTopicARN  string
+	BufferInDays int
+}
+
+func getEnv() (Env, error) {
+	domainName, err := GetDomainName()
+	if err != nil {
+		return Env{}, err
+	}
+	snsTopicARN, err := GetSNSTopicARN()
+	if err != nil {
+		return Env{}, err
+	}
+	bufferInDays, err := GetBufferInDays()
+	if err != nil {
+		return Env{}, err
+	}
+	return Env{
+		DomainName:   domainName,
+		SNSTopicARN:  snsTopicARN,
+		BufferInDays: bufferInDays,
+	}, nil
+}
+
+func GetDomainName() (string, error) {
+	domainName := os.Getenv("DOMAIN_NAME")
+	if domainName == "" {
+		return "", ErrDomainName
+	}
+	return domainName, nil
+}
+
+func GetSNSTopicARN() (string, error) {
+	snsTopicARN := os.Getenv("SNS_TOPIC_ARN")
+	if snsTopicARN == "" {
+		return "", ErrSNSTopicARN
+	}
+	return snsTopicARN, nil
+}
+
+func GetBufferInDays() (int, error) {
+	bufferInDays := os.Getenv("BUFFER_IN_DAYS")
+	if bufferInDays == "" {
+		return 0, ErrBufferInDays
+	}
+	days, err := parseInt(bufferInDays)
+	if err != nil {
+		return 0, err
+	}
+	return days, nil
+}
+
+type CertInfo struct {
+	Expiration     time.Time
+	Buffer         time.Time
+	CertDiffInDays int
+}
+
+func parseInt(str string) (int, error) {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+type Dialer interface {
+	Dial(network string, addr string, config *tls.Config) (*tls.Conn, error)
+}
+
+func getConn(dial Dialer, domainName string) (*tls.Conn, error) {
+	conn, err := dial.Dial(
+		"tcp",
+		fmt.Sprintf("%s:443", domainName),
+		&tls.Config{InsecureSkipVerify: true})
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+type Connection interface {
+	ConnectionState() tls.ConnectionState
+}
+
+func getCertInfo(conn Connection, env Env) CertInfo {
+	expirationDate := conn.ConnectionState().PeerCertificates[0].NotAfter
+	now := time.Now()
+	buffer := now.Add((time.Hour * 24) * time.Duration(env.BufferInDays))
+	certDiffInDays := int(expirationDate.Sub(now).Hours() / 24)
+	return CertInfo{
+		Expiration:     expirationDate,
+		Buffer:         buffer,
+		CertDiffInDays: certDiffInDays,
+	}
+}
+
+func constructPubInput(env Env, certInfo CertInfo) *sns.PublishInput {
+	msg := fmt.Sprintf(
+		"%s certificate will expire in %d days on %s.",
+		env.DomainName,
+		certInfo.CertDiffInDays,
+		certInfo.Expiration.Format(time.RubyDate),
+	)
+	sub := fmt.Sprintf(
+		"%s Certificate Expiring Soon",
+		env.DomainName,
+	)
+	return &sns.PublishInput{
+		Message:  aws.String(msg),
+		Subject:  aws.String(sub),
+		TopicArn: aws.String(env.SNSTopicARN),
+	}
+}
+
+func pub(ctx context.Context, api SNSPublish, input *sns.PublishInput) (string, error) {
+	output, err := api.Publish(ctx, input)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("MessageID: %s", aws.ToString(output.MessageId)), nil
+}
+
+type TLSDialer struct{}
+
+func (t TLSDialer) Dial(network string, addr string, config *tls.Config) (*tls.Conn, error) {
+	return tls.Dial(network, addr, config)
+}
+
+func handler() (string, error) {
+	env, err := getEnv()
+	if err != nil {
+		return "", err
+	}
+
+	dial := TLSDialer{}
+	conn, err := getConn(dial, env.DomainName)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	certInfo := getCertInfo(conn, env)
+
+	// Break early when certificate is good
+	if certInfo.Buffer.After(certInfo.Expiration) {
+		return "", nil
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return "", err
+	}
+	client := sns.NewFromConfig(cfg)
+	input := constructPubInput(env, certInfo)
+	return pub(context.Background(), client, input)
+}
+
+func main() {
+	lambdaStart(handler)
+}
---
+++ certificate_checker_go/main_test.go
@@ -0,0 +1,280 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+)
+
+var (
+	domain = "example.com"
+	arn    = "arn:aws:sns:us-east-1:000000000000:blah"
+	bid    = "30"
+)
+
+func mockLambdaStart(i interface{}) {}
+
+func resetEnv() {
+	os.Setenv("DOMAIN_NAME", "")
+	os.Setenv("SNS_TOPIC_ARN", "")
+	os.Setenv("BUFFER_IN_DAYS", "")
+}
+
+func Test_main(t *testing.T) {
+	t.Run("main", func(t *testing.T) {
+		var testLambdaStart = lambdaStart
+		defer func() {
+			lambdaStart = testLambdaStart
+		}()
+		lambdaStart = mockLambdaStart
+		main()
+	})
+}
+
+func TestEnv(t *testing.T) {
+	// Testing DomainName
+	t.Run("Test DomainName success", func(t *testing.T) {
+		resetEnv()
+		os.Setenv("DOMAIN_NAME", domain)
+		os.Setenv("SNS_TOPIC_ARN", arn)
+		os.Setenv("BUFFER_IN_DAYS", bid)
+		env, err := getEnv()
+		if err != nil {
+			t.Fail()
+		}
+		if env.DomainName != domain {
+			t.Fail()
+		}
+	})
+	t.Run("Test DomainName failure", func(t *testing.T) {
+		resetEnv()
+		os.Setenv("DOMAIN_NAME", "")
+		os.Setenv("SNS_TOPIC_ARN", arn)
+		os.Setenv("BUFFER_IN_DAYS", bid)
+		env, err := getEnv()
+		if !errors.Is(err, ErrDomainName) {
+			t.Fail()
+		}
+		if env.DomainName != "" {
+			t.Fail()
+		}
+	})
+
+	// Testing SNSTopicARN
+	t.Run("Test SNSTopicARN success", func(t *testing.T) {
+		resetEnv()
+		os.Setenv("DOMAIN_NAME", domain)
+		os.Setenv("SNS_TOPIC_ARN", arn)
+		os.Setenv("BUFFER_IN_DAYS", bid)
+		env, err := getEnv()
+		if err != nil {
+			t.Fail()
+		}
+		if env.SNSTopicARN != arn {
+			t.Fail()
+		}
+	})
+	t.Run("Test SNSTopicARN failure", func(t *testing.T) {
+		resetEnv()
+		os.Setenv("DOMAIN_NAME", domain)
+		os.Setenv("SNS_TOPIC_ARN", "")
+		os.Setenv("BUFFER_IN_DAYS", bid)
+		env, err := getEnv()
+		if !errors.Is(err, ErrSNSTopicARN) {
+			t.Fail()
+		}
+		if env.SNSTopicARN != "" {
+			t.Fail()
+		}
+	})
+
+	// Testing BufferInDays
+	t.Run("Test BufferInDays success", func(t *testing.T) {
+		resetEnv()
+		os.Setenv("DOMAIN_NAME", domain)
+		os.Setenv("SNS_TOPIC_ARN", arn)
+		os.Setenv("BUFFER_IN_DAYS", bid)
+		env, err := getEnv()
+		if err != nil {
+			t.Fail()
+		}
+		if env.BufferInDays != 30 {
+			t.Fail()
+		}
+	})
+	t.Run("Test BufferInDays empty failure", func(t *testing.T) {
+		resetEnv()
+		os.Setenv("DOMAIN_NAME", domain)
+		os.Setenv("SNS_TOPIC_ARN", arn)
+		os.Setenv("BUFFER_IN_DAYS", "")
+		env, err := getEnv()
+		if !errors.Is(err, ErrBufferInDays) {
+			t.Fail()
+		}
+		if env.SNSTopicARN != "" {
+			t.Fail()
+		}
+	})
+	t.Run("Test BufferInDays parse int failure", func(t *testing.T) {
+		resetEnv()
+		os.Setenv("DOMAIN_NAME", domain)
+		os.Setenv("SNS_TOPIC_ARN", arn)
+		os.Setenv("BUFFER_IN_DAYS", "a")
+		env, err := getEnv()
+		if err == nil {
+			t.Fail()
+		}
+		if env.SNSTopicARN != "" {
+			t.Fail()
+		}
+	})
+}
+
+type MockDialSuccess struct{}
+
+func (d MockDialSuccess) Dial(network string, addr string, config *tls.Config) (*tls.Conn, error) {
+	return &tls.Conn{}, nil
+}
+
+type MockDialFail struct{}
+
+func (d MockDialFail) Dial(network string, addr string, config *tls.Config) (*tls.Conn, error) {
+	return nil, errors.New("fail")
+}
+
+func Test_getConn(t *testing.T) {
+	t.Run("get connection success", func(t *testing.T) {
+		dial := MockDialSuccess{}
+		_, err := getConn(dial, "example.com")
+		if err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("get connection fail", func(t *testing.T) {
+		dial := MockDialFail{}
+		_, err := getConn(dial, "example.com")
+		if err == nil {
+			t.Fail()
+		}
+	})
+}
+
+type MockConn struct{}
+
+func (c MockConn) ConnectionState() tls.ConnectionState {
+	return tls.ConnectionState{
+		PeerCertificates: []*x509.Certificate{
+			{
+				NotAfter: time.Now(),
+			},
+		},
+	}
+}
+
+func Test_getCertInfo(t *testing.T) {
+	conn := MockConn{}
+	resetEnv()
+	os.Setenv("DOMAIN_NAME", domain)
+	os.Setenv("SNS_TOPIC_ARN", arn)
+	os.Setenv("BUFFER_IN_DAYS", bid)
+	env, _ := getEnv()
+	certInfo := getCertInfo(conn, env)
+	if certInfo.Buffer == time.Now() {
+		t.Fail()
+	}
+}
+
+func Test_constructPubInput(t *testing.T) {
+	conn := MockConn{}
+	resetEnv()
+	os.Setenv("DOMAIN_NAME", domain)
+	os.Setenv("SNS_TOPIC_ARN", arn)
+	os.Setenv("BUFFER_IN_DAYS", bid)
+	env, _ := getEnv()
+	certInfo := getCertInfo(conn, env)
+	input := constructPubInput(env, certInfo)
+	if input == nil {
+		t.Fail()
+	}
+}
+
+type mockSNSSuccess struct{}
+
+func (m mockSNSSuccess) Publish(context.Context, *sns.PublishInput, ...func(*sns.Options)) (*sns.PublishOutput, error) {
+	return &sns.PublishOutput{}, nil
+}
+
+type mockSNSFail struct{}
+
+func (m mockSNSFail) Publish(context.Context, *sns.PublishInput, ...func(*sns.Options)) (*sns.PublishOutput, error) {
+	return nil, errors.New("failed")
+}
+
+func Test_pub(t *testing.T) {
+	t.Run("Publish successfully", func(t *testing.T) {
+		resetEnv()
+		m := mockSNSSuccess{}
+		_, err := pub(context.Background(), m, &sns.PublishInput{})
+		if err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("Fail to publish", func(t *testing.T) {
+		resetEnv()
+		m := mockSNSFail{}
+		_, err := pub(context.Background(), m, &sns.PublishInput{})
+		if err == nil {
+			t.Fail()
+		}
+	})
+}
+
+func Test_handler(t *testing.T) {
+	t.Run("general failure", func(t *testing.T) {
+		os.Setenv("DOMAIN_NAME", domain)
+		os.Setenv("SNS_TOPIC_ARN", arn)
+		os.Setenv("BUFFER_IN_DAYS", bid)
+		_, err := handler()
+		if err == nil {
+			t.Fail()
+		}
+	})
+	t.Run("env failure", func(t *testing.T) {
+		resetEnv()
+		_, err := handler()
+		if err == nil {
+			t.Fail()
+		}
+	})
+	t.Run("dial", func(t *testing.T) {
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "hello")
+			})
+			fmt.Println(http.ListenAndServeTLS(":443", "localhost.crt", "localhost.key", nil))
+		}()
+		go func() {
+			defer wg.Done()
+			resetEnv()
+			os.Setenv("DOMAIN_NAME", "localhost")
+			os.Setenv("SNS_TOPIC_ARN", arn)
+			os.Setenv("BUFFER_IN_DAYS", bid)
+			_, err := handler()
+			if err == nil {
+				t.Fail()
+			}
+		}()
+		wg.Wait()
+	})
+}
